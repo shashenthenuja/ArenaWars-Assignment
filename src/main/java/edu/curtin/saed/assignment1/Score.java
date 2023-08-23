b127c1eb9cb0a42b6cb6620876b424f5e56c683a
Shashen Thenuja <shashengg@gmail.com>
2023-08-23 22:54:40 +0530

    Create Score.java

---
+++ src/main/java/edu/curtin/saed/assignment1/Score.java
@@ -0,0 +1,14 @@
+package edu.curtin.saed.assignment1;
+
+public class Score {
+    private int score = 0;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore() {
+        score += 10;
+    }
+
+}
